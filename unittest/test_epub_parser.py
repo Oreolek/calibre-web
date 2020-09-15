13e4e90db4dd4d06c79128254cbbc86726bfb410
Alexander Yakovlev <keloero@oreolek.ru>
2020-09-15 17:23:10 +0700

    use input.epub

--- unittest/test_epub_parser.py
+++ unittest/test_epub_parser.py
@@ -8,5 +8,5 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vendor
 from modules import epub
 use_epub_meta = True
 
-meta = epub.get_epub_info('./pg63206-images.epub', 'imported.epub', 'epub')
+meta = epub.get_epub_info('./input.epub', 'imported.epub', 'epub')
 print(meta)
